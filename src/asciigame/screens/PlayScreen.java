599bea1620aad11f133a0bdf55512f77cf252064
Austin <adecker5@ycp.edu>
2015-05-29 08:44:42 -0400

    Reverted the messaging system so I can start fresh with actual good ideas.

--- src/asciigame/screens/PlayScreen.java
+++ src/asciigame/screens/PlayScreen.java
@@ -7,15 +7,8 @@ import asciigame.WorldBuilder;
 import asciigame.creatures.Creature;
 import asciigame.creatures.CreatureFactory;
 import java.awt.event.KeyEvent;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class PlayScreen implements Screen {
-
-	private static Deque<String> messages;
-    private static Deque<String> toDisplay;
-
-	public static void addMessage(String message) { messages.addLast(message); }
 
 	private int screenWidth;
 	private int screenHeight;
@@ -25,9 +18,6 @@ public class PlayScreen implements Screen {
 	private Creature player;
 
 	public PlayScreen() {
-		messages = new ArrayDeque<>();
-		toDisplay = new ArrayDeque<>();
-		addMessage("Welcome!");
 		screenWidth = ApplicationMain.getScreenWidth();
 		screenHeight = ApplicationMain.getScreenHeight();
 		rightMapBorder = screenWidth * 2 / 3;
@@ -52,7 +42,6 @@ public class PlayScreen implements Screen {
 		int left = getScrollX();
 		int top = getScrollY();
 		displayTiles(terminal, left, top);
-		displayMessages(terminal);
 		world.update();
 		String stats = player.getHealth() + "/" + player.getMaxHealth();
 		terminal.write(stats, 1, terminal.getHeightInCharacters() - 2);
@@ -130,65 +119,5 @@ public class PlayScreen implements Screen {
 				terminal.write(c.getGlyph(), screenX, screenY, c.getColor());
 			}
 		}
-	}
-
-	private void displayMessages(AsciiPanel terminal) {
-		int messageX = rightMapBorder + 1;
-		int messageY = 1;
-        String message;
-        toDisplay = new ArrayDeque<>(messages);
-
-		while (!toDisplay.isEmpty()) {
-			message = toDisplay.removeFirst();
-            message = trimString(message, screenWidth - messageX);
-			terminal.write(message, messageX, messageY);
-			messageY++;
-
-            if (messageY > screenHeight - 2) {
-                toDisplay.clear();
-                messages.clear();
-            }
-		}
-	}
-
-	private String trimString(String string, int maxLength) {
-
-        // Don't do anything if the string is already short enough
-        if (string.length() > maxLength) {
-
-            // Determine word breaks
-            String[] splitBySpace = string.split(" ");
-            if (splitBySpace.length == 1) {
-
-                // If there are no spaces, just truncate the word at maxLength
-                string = string.substring(0, maxLength);
-                toDisplay.addFirst(string.substring(maxLength));
-            } else {
-
-                // Gather every word that fits until the word that takes it over maxLength
-                int i = 0;
-                int sum = 0;
-                string = "";
-                do {
-                    string += splitBySpace[i] + " ";
-                    sum += splitBySpace[i].length() + 1;
-                    i++;
-                } while (i < splitBySpace.length && sum < maxLength);
-
-                // Remove that last word
-                i--;
-                sum -= splitBySpace[i].length() + 1;
-                string = string.substring(0, sum);
-
-                // Stitch together remaining words, if any, and push them back on the Deque
-                String remaining = "";
-                for (int j = i; j < splitBySpace.length; j++) {
-                    remaining += splitBySpace[j] + " ";
-                }
-                toDisplay.addFirst(remaining);
-            }
-        }
-
-		return string;
 	}
 }
