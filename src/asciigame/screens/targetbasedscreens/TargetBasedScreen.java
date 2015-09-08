c715196653c08447e3a98ac6185b987c3739eee2
Austin <adecker5@ycp.edu>
2015-09-08 15:57:41 -0400

    Added fields and constructor to TargetBasedScreen.

--- src/asciigame/screens/targetbasedscreens/TargetBasedScreen.java
+++ src/asciigame/screens/targetbasedscreens/TargetBasedScreen.java
@@ -1,19 +1,33 @@
 package asciigame.screens.targetbasedscreens;
 
 import asciiPanel.AsciiPanel;
+import asciigame.creatures.Creature;
 import asciigame.screens.Screen;
-
 import java.awt.event.KeyEvent;
 
 public class TargetBasedScreen implements Screen {
 
-	@Override
-	public Screen respondToUserInputAndUpdate(KeyEvent key) {
-		return null;
+	protected Creature player;
+	protected String caption;
+	private int offsetX;
+	private int offsetY;
+	private int x;
+	private int y;
+
+	public TargetBasedScreen(Creature player, String caption, int offsetX, int offsetY) {
+		this.player = player;
+		this.caption = caption;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
 	}
 
 	@Override
 	public void displayOutput(AsciiPanel terminal) {
 
+	}
+
+	@Override
+	public Screen respondToUserInputAndUpdate(KeyEvent key) {
+		return null;
 	}
 }
